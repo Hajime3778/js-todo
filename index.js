1616c3bc75342c8d1cb729b4e763b77cc9d7978e
hajime.takeuchi <shadowthe8156@gmail.com>
2022-03-12 10:39:30 +0900

    完成形サンプル

--- index.js
+++ index.js
@@ -9,17 +9,25 @@ const todoList = document.getElementById("todo_list");
 
 // 追加ボタンクリック時のイベントを登録
 todoAddButton.addEventListener('click', () => {
-  const todo = todoInputText.value;
-  
-  addTodo();
+
+  // 入力した内容が空だったら
+  if (todoInputText.value == "") {
+    alert("TODOを入力してください");
+    return;
+  }
+
+  // TODO作成処理
+  addTodo(todoInputText.value);
+
+  // 入力初期化
+  todoInputText.value = "";
 });
 
-// TODO追加処理
 function addTodo(todo) {
   // liを作成します
   const listItem = document.createElement("li");
   listItem.className = "todo_li";
-  listItem.innerHTML = "ほげほげ";
+  listItem.innerHTML = todo;
 
   // 削除ボタンを作成します
   const deleteButton = document.createElement("button");
@@ -30,4 +38,9 @@ function addTodo(todo) {
   
   // ulに対して liを追加します
   todoList.appendChild(listItem);
+
+  // 削除ボタンクリック時のイベントを登録する
+  deleteButton.addEventListener('click', () => {
+    todoList.removeChild(listItem);
+  });
 }
